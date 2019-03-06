45e0d93d66949c17e83d998fce0de07cb7e2cee0
Matthew Leibowitz <mattleibow@live.com>
2019-03-06 20:28:01 +0200

    Some hacks to get things cracking

--- source/androidx.tvprovider/tvprovider/additions/Additions.cs
+++ source/androidx.tvprovider/tvprovider/additions/Additions.cs
@@ -2,12 +2,4 @@
 
 namespace AndroidX.TvProvider.Media.TV
 {
-	public partial class Program
-	{
-		//public int CompareTo(global::AndroidX.TvProvider.Media.TV.Program other)
-		//{
-		//	return CompareTo((global::Java.Lang.Object)other);
-		//}
-
-	}
 }
